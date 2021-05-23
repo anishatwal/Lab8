5480a2ba0a6251e5d6bdaa0a47659e278a00fa41
Anisha <anishaatwal@yahoo.com>
2021-05-23 15:01:59 -0700

    finished lab

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,26 @@
 /**
  * @jest-environment jsdom
  */
+import { pushToHistory } from '../scripts/router.js';
+
+describe('pushToHistory function', () => {
+    test('settings case', () => {
+        let newState = pushToHistory('settings', 0);
+        expect(newState.length).toBe(history.length);
+        expect(newState.state).toEqual({ page: 'settings' });
+    });
+
+    test('entry case', () => {
+        let newState = pushToHistory('entry', 2);
+        expect(newState.length).toBe(history.length);
+        expect(newState.state).toEqual({ page: 'entry2' });
+    });
+
+    test('default case', () => {
+        let newState = pushToHistory('', 0);
+        expect(newState.length).toBe(history.length);
+        expect(newState.state).toEqual({});
+    });
+});
+
 
